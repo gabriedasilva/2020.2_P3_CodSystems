c08afb1643dd0531626d3678bf149af3a6738b20
Gabriel <multimakerimg@gmail.com>
2020-09-27 21:32:30 -0300

    (Correção)Tela de Login (Mobile)

--- Mobile/iClass/App.js
+++ Mobile/iClass/App.js
@@ -3,44 +3,96 @@ import { View, KeyboardAvoidingView, Image, TextInput, TouchableOpacity, StyleSh
 
 const App = () => {
   return (
-    
-<KeyboardAvoidingView style={styles.background}>
-      <View>
+
+    <KeyboardAvoidingView style={styles.background}>
+      <View style={styles.imgContainer}>
         <Image
-          source={require('./src/assets/logo.png')}
+
+          source={require('./src/assets/logo256w.png')}
         />
       </View>
-      <View>
+
+      <View style={styles.container}>
+
         <TextInput
-          placeholder="Email"
+          style={styles.input}
+          placeholder="Matrícula"
           autoCorrect={false}
-          onChangeText={() => { }}
         />
 
         <TextInput
+            style={styles.input}
           placeholder="Senha"
           autoCorrect={false}
-          onChangeText={() => { }}
         />
 
-        <TouchableOpacity>
-          <Text onPress={() => {}}>Acessar</Text>
+        <TouchableOpacity style={styles.btnSubmit}>
+          <Text style={styles.submitText}
+          onPress={() => { }}>Acessar</Text>
         </TouchableOpacity>
+
         <TouchableOpacity>
-          <Text onPress={() => {}}>Esqueci a Senha!</Text>
+          <Text style={styles.textWhiteL} onPress={() => { }}>Esqueci a Senha!</Text>
         </TouchableOpacity>
+
       </View>
-      </KeyboardAvoidingView>
- 
-  );
+      <View style={styles.emptyContainer}></View>
+      <View>
+<Text style={styles.textWhiteL}>iClass - CodSystems 2020</Text>
+      </View>
+    </KeyboardAvoidingView>
+
+  )
 };
 const styles = StyleSheet.create({ // Estilos
-  background:{
+  background: {
     flex: 1,
-    alignContent: 'center',
+    alignItems: 'center',
     justifyContent: 'center',
-    backgroundColor: '#191919'
+    backgroundColor: '#2196f3'
+  },
+  imgContainer: {
+    justifyContent: 'center',
+    flex: 1,
+  },
+  container: {
+    backgroundColor:'#212121',
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '90%',
+    borderRadius:30,
+
+  },
+  input:{
+    backgroundColor:'#EEEEEE',
+    width:'90%',
+    borderRadius:10,
+    marginBottom:10,
+    fontSize:17,
+    padding:10,
+    color:'#222',
+  },
+  textWhiteL:{
+color:'#fff'
+  },
+  btnSubmit:{
+    backgroundColor:'#009900',
+    padding:10,
+    width:'90%',
+    justifyContent:'center',
+    alignItems:'center',
+    borderRadius:10
+  },
+  submitText:{
+color:'#fff',
+fontSize:20
+  },
+  emptyContainer:{
+  height:'20%'
   }
+  
+
 });
 
 export default App;
